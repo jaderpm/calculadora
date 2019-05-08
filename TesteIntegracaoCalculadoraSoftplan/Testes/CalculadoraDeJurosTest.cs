f167d774378f39cd1734260d91bfb77e80b8845d
jaderpm <jaderpm@hotmail.com>
2019-05-08 07:45:56 -0300

    Adicionando testes de integração v1.0

---
+++ TesteIntegracaoCalculadoraSoftplan/Testes/CalculadoraDeJurosTest.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TesteIntegracaoCalculadoraSoftplan.Testes
+{
+    public class CalculadoraDeJurosTest
+    {
+        private Contexto.Contexto _contexto;
+        public CalculadoraDeJurosTest()
+        {
+            _contexto = new Contexto.Contexto();
+        }
+
+        [Fact]
+        public async Task TestaFuncionamentoCalculaJuros()
+        {
+            var response = await _contexto.Client.GetAsync("/CalculaJuros?valorInicial=100&meses=5");
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task TestaFuncionamentoCalculaJurosLetrasMinusculas()
+        {
+            var response = await _contexto.Client.GetAsync("/calculajuros?valorInicial=100&meses=5");
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task TestaChamadaErrada()
+        {
+            var response = await _contexto.Client.GetAsync("/calculajuro");
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task TestaEnvioFormatoIncorretoCaracteres()
+        {
+            var response = await _contexto.Client.GetAsync("/calculajuros?valorInicial=r&meses=5");
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [Fact]
+        public async Task TestaEnvioFormatoIncorretoSemVariavelErroTratado()
+        {
+            var response = await _contexto.Client.GetAsync("/calculajuros?&meses=5");
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task TestaTipoRetorno()
+        {
+            var response = await _contexto.Client.GetAsync("/calculajuros?valorInicial=100&meses=5");
+            response.EnsureSuccessStatusCode();
+            response.Content.Headers.ContentType.ToString().Should().Be("text/plain; charset=utf-8");
+        }
+
+    }
+}
---
+++ TesteIntegracaoCalculadoraSoftplan/Testes/ShowMeTheCodeTeste.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace TesteIntegracaoCalculadoraSoftplan.Testes
+{
+    public class ShowMeTheCodeTeste
+    {
+        private Contexto.Contexto _contexto;
+
+        public ShowMeTheCodeTeste()
+        {
+            _contexto = new Contexto.Contexto();
+        }
+
+        [Fact]
+        public async Task TestaFuncionamento()
+        {
+            var response = await _contexto.Client.GetAsync("/ShowMeTheCode");
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        [Fact]
+        public async Task TestaChamadaErrada()
+        {
+            var response = await _contexto.Client.GetAsync("/ShowMeTheCo");
+            response.StatusCode.Should().Be(HttpStatusCode.NotFound);
+        }
+
+        [Fact]
+        public async Task TestaTipoRetorno()
+        {
+            var response = await _contexto.Client.GetAsync("ShowMeTheCode");
+            response.EnsureSuccessStatusCode();
+            response.Content.Headers.ContentType.ToString().Should().Be("text/plain; charset=utf-8");
+        }
+        [Fact]
+        public async Task TestaFuncionamentoChamadaLetrasMinusculas()
+        {
+            var response = await _contexto.Client.GetAsync("/showmethecode");
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+    }
+}
