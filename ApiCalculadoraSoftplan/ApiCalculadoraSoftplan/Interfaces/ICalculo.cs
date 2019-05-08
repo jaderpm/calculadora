99d573795a651f2ae71f12f3b54e0a08c611a3bc
jaderpm <jaderpm@hotmail.com>
2019-05-07 22:36:36 -0300

    Adicionando interfaces de calculo e formato de retorno v1.0

---
+++ ApiCalculadoraSoftplan/ApiCalculadoraSoftplan/Interfaces/ICalculo.cs
@@ -0,0 +1,13 @@
+﻿using ApiCalculadoraSoftplan.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCalculadoraSoftplan.Interfaces
+{
+    public interface ICalculo
+    {
+        double Calcular(Calculo p_calculo); 
+    }
+}
---
+++ ApiCalculadoraSoftplan/ApiCalculadoraSoftplan/Interfaces/IFormatoRetorno.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCalculadoraSoftplan.Interfaces
+{
+    public interface IFormatoRetorno
+    {
+        string Formatar(double p_valor);  
+    }
+}
