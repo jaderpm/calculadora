0f4c7eb40eff0739c65ea86a25150eb91d981d7d
jaderpm <jaderpm@hotmail.com>
2019-05-07 22:39:28 -0300

    Adicionando Factories de calculo e formato de retorno v1.0

---
+++ ApiCalculadoraSoftplan/ApiCalculadoraSoftplan/Factories/FactoryCalculo.cs
@@ -0,0 +1,23 @@
+﻿using ApiCalculadoraSoftplan.Interfaces;
+using ApiCalculadoraSoftplan.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCalculadoraSoftplan.Factories
+{
+    public enum E_calculo
+    {
+        Juros = 1
+    }
+    public class FactoryCalculo
+    {
+        public static ICalculo Criar(E_calculo p_eCaulculo)
+        {
+            if (p_eCaulculo == E_calculo.Juros)
+                return new CalculoJurosService(); 
+            return new CalculoJurosService();
+        }
+    }
+}
---
+++ ApiCalculadoraSoftplan/ApiCalculadoraSoftplan/Factories/FactoryFormatoRetorno.cs
@@ -0,0 +1,25 @@
+﻿using ApiCalculadoraSoftplan.Interfaces;
+using ApiCalculadoraSoftplan.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiCalculadoraSoftplan.Factories
+{
+    public class FactoryFormatoRetorno
+    {
+        public enum E_formato
+        {
+            FormatoComVirgulaEDuasCasasDecimais = 1
+        }
+
+        public static IFormatoRetorno Criar(E_formato p_eFormato)
+        {
+            if (p_eFormato == E_formato.FormatoComVirgulaEDuasCasasDecimais)
+                return new FormatoComVirgulaEDuasCasasDecimaisService();  
+            return new FormatoComVirgulaEDuasCasasDecimaisService();
+        }
+
+    }
+}
